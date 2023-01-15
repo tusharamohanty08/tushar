c5c39e9e63b889c31112573c7307c39778d79ad8
tusharamohanty08 <121556411+tusharamohanty08@users.noreply.github.com>
2023-01-15 14:06:02 +0530

    Add files via upload

---
+++ concatenate.java
@@ -0,0 +1,8 @@
+class concatenate{
+    public static void main(String args[]) {
+
+        String s= "TUSHARA" + "MOHANTY" ;
+        System.out.println(s);
+        
+    }
+}
---
+++ mitan.java
@@ -0,0 +1,10 @@
+class details{
+    public static void main(String args[]) {
+
+        String s= "Mitan";
+        int n=5;
+        for (int i=0;i<5;i++){
+            System.out.println(s);
+        }
+    }
+}
